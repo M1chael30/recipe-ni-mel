62e0b7c0ef3d3884e70d3a5165763778dfe1a26d
Jilo <tejanomichael41@gmail.com>
2025-10-14 20:43:19 +0800

    fix responsiveness cuisine card

--- app/cuisines/[cuisine]/page.js
+++ app/cuisines/[cuisine]/page.js
@@ -20,7 +20,7 @@ export default function Cuisine() {
    </div>
 
    {/* cuisine card */}
-   <div className="grid auto-rows-min grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3">
+   <div className="grid auto-rows-min grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
     {/* dito mag mamap */}
     {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
      <Link
