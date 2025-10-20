5df360759f5a10d5c55383abf119e66ea6bc9488
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2025-10-20 22:22:40 +0800

    refactor about-us.jsx

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,6 +1,6 @@
 import Header from "@/components/about-us/header";
-import MyProfile from "../../public/profiles/angge.png";
-import Image from "next/image";
+import { HoverCardProfile } from "@/components/about-us/hover-card-profile";
+import { teamData } from "@/lib/data";
 
 export default function AboutUs() {
  return (
@@ -8,43 +8,13 @@ export default function AboutUs() {
    {/* title */}
    <Header />
 
-   {/* <div
-    className={
-     "relative flex size-40 shrink-0 overflow-hidden rounded-lg shadow-lg cursor-pointer"
-    }
-   >
-    <Image
-     priority
-     alt="my profile"
-     src={MyProfile}
-     className="absolute inset-0 object-cover aspect-square size-full"
-    />
-    <img
-     alt="my profile"
-     src={"/my_profile2.jpg"}
-     className="absolute inset-0 aspect-square size-full object-cover hover:opacity-0 transition-opacity"
-    />
-   </div> */}
-
    {/*content */}
    <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-    <div className="bg-orange-ni-carmel aspect-square relative flex shrink-0 overflow-hidden rounded-lg shadow-lg cursor-pointer">
-     <Image
-      priority
-      alt="my profile"
-      src={MyProfile}
-      className="absolute inset-0 object-cover aspect-square size-full"
-     />
-     <img
-      alt="my profile"
-      src={"/profiles/angela.jpg"}
-      className="absolute inset-0 aspect-square size-full object-cover hover:opacity-0 transition-opacity"
-     />
-    </div>
-    <div className="bg-orange-ni-carmel aspect-square rounded-lg" />
-    <div className="bg-orange-ni-carmel aspect-square rounded-lg" />
-    <div className="bg-orange-ni-carmel aspect-square rounded-lg" />
-    <div className="bg-orange-ni-carmel aspect-square rounded-lg" />
+    <HoverCardProfile item={teamData[0]} />
+    <HoverCardProfile />
+    <HoverCardProfile />
+    <HoverCardProfile />
+    <HoverCardProfile />
    </div>
   </section>
  );
---
+++ components/about-us/hover-card-profile.jsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+ HoverCard,
+ HoverCardContent,
+ HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import ProfileCard from "./profile-card";
+
+export function HoverCardProfile({ item = {} }) {
+ return (
+  <HoverCard>
+   <HoverCardTrigger>
+    <ProfileCard item={item} />
+   </HoverCardTrigger>
+   <HoverCardContent className="w-80">
+    <div className="flex gap-4">
+     <Avatar>
+      <AvatarImage src={item.img2} />
+      <AvatarFallback>{item.name}</AvatarFallback>
+     </Avatar>
+     <div className="space-y-1">
+      <h4 className="text-sm font-semibold">{item.name}</h4>
+      <p className="text-sm">
+       {item.role}
+      </p>
+      <div className="text-muted-foreground text-xs">{item.socials}</div>
+     </div>
+    </div>
+   </HoverCardContent>
+  </HoverCard>
+ );
+}
